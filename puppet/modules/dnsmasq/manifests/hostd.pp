b1b370c7f4739614c307645c4c5218864ae093d1
Michael Coughlin <michael.coughlin@colorado.edu>
2014-11-18 12:41:57 -0700

    removed some uneeded modules

--- puppet/modules/dnsmasq/manifests/hostd.pp
+++
@@ -1,8 +0,0 @@
-define dnsmasq::hostd ($source=undef, $content=undef) {
-
-	file {"/etc/dnsmasq-hosts.d/$name":
-		source  => $source,
-		content => $content,
-		notify  => Service["dnsmasq"],
-	}
-}
