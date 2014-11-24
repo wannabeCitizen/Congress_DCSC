b129dd3f694cc4ba3eb2961a3dcebc3b4d1557a0
Michael Coughlin <michael.coughlin@colorado.edu>
2014-11-24 14:47:20 -0700

    removed unneeded packages

--- puppet/modules/common_config/manifests/init.pp
+++ puppet/modules/common_config/manifests/init.pp
@@ -10,6 +10,16 @@ class common_config(){
 		homepath		=>	'/home',
 	}
 
+	accounts{ 'michael':
+		uname		=>  'michael',
+		uid             =>  1006,
+		realname        =>  'Michael Coughlin',
+		sshkeytype      =>  'ssh-rsa',
+		sshkey          =>  'AAAAB3NzaC1yc2EAAAADAQABAAABAQC9LAFcXxkonD5CAXzPTtbp/zbUBmOQbRE5U4QpfK0N0+PGU7wsjoPEd2BSQRgASMIVypbW2oRvtWjMqDGo70iBh3sSnaogLIZS4Z1+8zlSZ33bSqcWFXc6ORe8F9qMiV8eKxKxei3Ix2lovxUZYsZKOR3pl4QGigotS5QaK9mdwtpZ03hLWFqh+w6dQC1pAvuwWGvEQuVLaEpXUxOarzPzhTckEva7veiW6EByvghR+/K+laLqP89oml2klPZJWVIbNLPrMYPCcTAmZIuU4UWGye6bESsUB5qPFdSXLqqtav4SyE7nhAyjxjzJAs7Bz2ZfXJfysXxBJB1DZvgLLFdd',
+		shell			=>	'/bin/bash',
+		homepath		=>	'/home',
+	}
+
 	accounts{ 'ginesh':
 		uname		     => "ginesh",
 		uid		       => 1006,
@@ -20,11 +30,15 @@ class common_config(){
 		homepath	   => '/home',
 	}
 
+	class{ 'apt':
+		always_apt_update => true,
+	}
 
-#	sudo::conf { 'admin':
-#		priority	=> 10,
-#		content		=> "%admin ALL=(ALL) NOPASSWD: ALL",
-#	}
+	class{'apt::unattended_upgrades':
+		update   => '1',
+		download => '1',
+		upgrade  => '1',
+	}
 
 	sudo::sudoers{ 'sudoers':
 		ensure	=> 'present',
@@ -41,7 +55,7 @@ class common_config(){
 		opt_misc		=> ['hlsearch', 'autoindent', 'ruler', 'visualbell', 'mouse=a', 'number'],
 	}
 
-	$packages_list = ["virt-manager", "qemu-kvm", "bash-completion", "htop", "iotop", "iftop", "openssh-server", "openjdk-7-jdk", "git", "build-essential", "autogen", "make", "curl", "rsync", "tcpdump", "ntp", "wget", "tmux", "screen", "vim-puppet"]
+	$packages_list = ["bash-completion", "htop", "iotop", "iftop", "openssh-server", "openjdk-7-jdk", "git", "make", "curl", "rsync", "tcpdump", "ntp", "wget", "tmux", "screen", "vim-puppet"]
 
 	package{$packages_list: ensure => "latest"}
 
