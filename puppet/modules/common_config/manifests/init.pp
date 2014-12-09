fba30d12a031fb0bcd21b63d5bd12d1cfbb68132
Michael Coughlin <michael.coughlin@colorado.edu>
2014-12-09 15:10:43 -0700

    added flask and redis python packages

--- puppet/modules/common_config/manifests/init.pp
+++ puppet/modules/common_config/manifests/init.pp
@@ -55,7 +55,7 @@ class common_config(){
 		opt_misc		=> ['hlsearch', 'autoindent', 'ruler', 'visualbell', 'mouse=a', 'number'],
 	}
 
-	$packages_list = ["bash-completion", "htop", "iotop", "iftop", "openssh-server", "git", "make", "rsync", "tcpdump", "ntp", "wget", "tmux", "screen", "vim-puppet", "avahi-utils"]
+	$packages_list = ["bash-completion", "htop", "iotop", "iftop", "openssh-server", "git", "make", "rsync", "tcpdump", "ntp", "wget", "tmux", "screen", "vim-puppet", "avahi-utils", "python-flask", "python-redis"]
 
 	package{$packages_list: ensure => "latest"}
 
