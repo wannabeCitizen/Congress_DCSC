b964e551e2babef16c5ecf4d5f232541fa7d1c75
wannabeCitizen <doorsofskirpan@gmail.com>
2014-12-14 19:11:33 -0700

    Basic RPC fulfillment on Worker

--- Worker.py
+++ Worker.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 import pika
+import redis
+
+redisHost = 'node1-database.local'
+redis_rawdoc = redis.Redis(host=redisHost, db=1) # Key = Raw Document File; Value = Body of Document - Raw
+redis_name= redis.Redis(host=redisHost, db=2) # Key = Speaker Name; Value = The Raw Doc File Name
+redis_title = redis.Redis(host=redisHost, db=3) # Key = License; Value = MD5 Hash
+redis_date = redis.Redis(host=redisHost, db=4) # Key = License; Value = File Name
 
 hostname = 'webserver.local'
 
@@ -18,13 +25,37 @@ response_types = {
 }
 
 def by_date(q_date):
-    pass
+    data = []
+    if redis_date.exists(q_date):
+        doc_list = redis_date.lrange(q_date, 0, -1)
+    for x in doc_list:
+        data.append(redis_rawdoc.get(x))
+    return data
+    
 
 def by_name(q_name):
-    pass
+    data = []
+    people = redis_name.keys('*'+q_name+'*')
+    for person in people:
+        doc_list = redis_name.lrange(person, 0, -1)
+        i = 0
+        for doc in doc_list:
+            if i < 10:
+                data.append(redis_rawdoc.get(doc))
+                i += 1
+    return data
 
 def by_doc(q_doc):
-    pass
+    data = []
+    docs_list = redis_title.keys('*'+q_doc+'*')
+    for title in doc_list:
+        docs = redis_doc.lrange(title, 0, -1)
+        i = 0
+        for doc in docs:
+            if i<10:
+                data.append(redis_rawdoc.get(doc))
+                i += 1
+    return data
 
 def search(q_string):
     pass
